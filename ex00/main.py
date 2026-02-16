627c5af1d47c2bfb3bea479d0ccc267582c7f171
hikami <tokyrakotomahefa@gmail.com>
2026-02-16 16:25:10 +0000

    add ex00

---
+++ ex00/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+ft_list[1] = "World!"
+ft_tuple = ("Hello", "Madagascar!")
+ft_set = list({"Hello", "Antananarivo!"})
+ft_dict["Hello"] = "42Antananarivo!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
