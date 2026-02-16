4e2b7070336c09be9288411a1438084e0bcc4cc4
hikami <tokyrakotomahefa@gmail.com>
2026-02-16 18:07:29 +0000

    add ex02 and make a gitignore file for __pycache__ directory

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,10 @@
+def all_thing_is_obj(object: any) -> int:
+    if type(object) is str:
+        print(f"{object} is in the kitchen : {type(object)}")
+        
+    elif type(object) is int:
+        print("Type not found")
+        
+    else:
+        print(type(object).__name__.capitalize(), ":", type(object))
+    return (42)
